e600bb918d907da791afafbdcbaf94bb23a0d34f
Arihant Kumar <kumar.arihant0607@gmail.com>
2026-01-12 17:47:16 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+  console.log("hitting /");
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
