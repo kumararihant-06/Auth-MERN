835dd66256599062f75bbe1eb44174aecdaef51d
Arihant Kumar <kumar.arihant0607@gmail.com>
2026-01-12 19:00:41 +0530

    Database setup completed.

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,19 @@
-const express = require("express");
-
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/connectDB.js";
+import authRoutes from "../backend/routes/auth.routes.js"
 const app = express();
+dotenv.config()
 
 app.get("/", (req, res) => {
   res.send("hello");
   console.log("hitting /");
 });
 
+app.use("/api/auth", authRoutes);
+
 app.listen(3000, () => {
-  console.log("server running on port 3000");
+  console.log(`Server running on port : ${process.env.PORT}`);
+  connectDB();
 });
+
